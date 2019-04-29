ca6250b2d43ac0d58557269e13fdc0ce05eb0182
Mihai Corciu <mc200520052005@yahoo.com>
2019-04-29 15:31:59 +0300

    correction

--- ml/convert.sh
+++ ml/convert.sh
@@ -1,2 +1,2 @@
 #jupyter nbconvert --to html test_latex.ipynb
-jupyter nbconvert --to html pytorch6.ipynb
+jupyter nbconvert --to html pytorch2.ipynb
--- statistics/convert.sh
+++ statistics/convert.sh
@@ -1,2 +1,2 @@
 #jupyter nbconvert --to html test_latex.ipynb
-jupyter nbconvert --to html introduction.ipynb
+jupyter nbconvert --to html linear_proof.ipynb
