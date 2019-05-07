a7cd7f68febfb1e8f260321544fd3540d753f089
Mihai <mihai@localhost.localdomain>
2019-05-08 00:27:04 +0300

    index

---
+++ list.py
@@ -0,0 +1,48 @@
+import os
+import re
+
+string = ""
+
+def traverse_dir(dir):
+
+    global string
+    string += '<ul>\n'
+
+    for item in os.listdir(dir):
+    #for item in filterList(os.listdir(dir)):
+
+        #exclude settings folder
+        #if(item != "debug"):
+        avoid = re.findall("^\.(.*)$",item)
+        if( avoid == []):
+
+            fullpath = os.path.join(dir, item)
+
+            if os.path.isdir(fullpath):
+                string += ('<li>%s</li>\n' % item)
+            else:
+
+                #only *.html and *.ipynb
+                html = re.findall("^(.*).html$",item)
+                ipynb = re.findall("^(.*).ipynb$",item)
+
+                if(html != [] or ipynb != []):
+                    #startStr = PWD + "/blog/templates/blog/data"
+                    startStr = "https://nbviewer.jupyter.org/github/mihai2014/python-data-processing/blob/master"
+                    #remove startStr
+                    #relativePath = fullpath[len(startStr):]
+                    fullpath = fullpath[1:len(fullpath)]
+                    relativePath = startStr + fullpath
+                    string += ('<li><a href="%s">%s</a></li>\n' % (relativePath,item))
+
+
+            if os.path.isdir(fullpath):
+                if os.listdir(fullpath) != []:
+                    traverse_dir(fullpath)
+
+    string += '</ul>\n'
+
+traverse_dir('.')
+print(string)
+
+#ttps://nbviewer.jupyter.org/github/mihai2014/python-data-processing/blob/master
